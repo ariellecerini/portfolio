1f0ba286589314918ed56bcd5cda7c711f3264cb
ariellecerini <arielle@acerinidesigns.com>
2024-07-14 02:04:59 -0400

    update token work page

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -4,19 +4,19 @@ import { normalizeURL, decode } from 'ufo'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _79d68257 = () => interopDefault(import('../pages/about.vue' /* webpackChunkName: "pages/about" */))
-const _908a12d6 = () => interopDefault(import('../pages/portfolio/index.vue' /* webpackChunkName: "pages/portfolio/index" */))
-const _ef6ff590 = () => interopDefault(import('../pages/test.vue' /* webpackChunkName: "pages/test" */))
-const _590505b4 = () => interopDefault(import('../pages/portfolio/communications-design/index.vue' /* webpackChunkName: "pages/portfolio/communications-design/index" */))
-const _064ca990 = () => interopDefault(import('../pages/portfolio/design-research/index.vue' /* webpackChunkName: "pages/portfolio/design-research/index" */))
-const _01c7fa3f = () => interopDefault(import('../pages/portfolio/ui-ux-design/index.vue' /* webpackChunkName: "pages/portfolio/ui-ux-design/index" */))
-const _0ed653a7 = () => interopDefault(import('../pages/portfolio/web-design/index.vue' /* webpackChunkName: "pages/portfolio/web-design/index" */))
-const _cce345c8 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
-const _574e646c = () => interopDefault(import('../pages/portfolio/communications-design/_slug.vue' /* webpackChunkName: "pages/portfolio/communications-design/_slug" */))
-const _04960848 = () => interopDefault(import('../pages/portfolio/design-research/_slug.vue' /* webpackChunkName: "pages/portfolio/design-research/_slug" */))
-const _001158f7 = () => interopDefault(import('../pages/portfolio/ui-ux-design/_slug.vue' /* webpackChunkName: "pages/portfolio/ui-ux-design/_slug" */))
-const _0d1fb25f = () => interopDefault(import('../pages/portfolio/web-design/_slug.vue' /* webpackChunkName: "pages/portfolio/web-design/_slug" */))
-const _93f75566 = () => interopDefault(import('../pages/portfolio/_slug.vue' /* webpackChunkName: "pages/portfolio/_slug" */))
+const _85e0f260 = () => interopDefault(import('../pages/about.vue' /* webpackChunkName: "pages/about" */))
+const _0ad11064 = () => interopDefault(import('../pages/portfolio/index.vue' /* webpackChunkName: "pages/portfolio/index" */))
+const _03dfe4c2 = () => interopDefault(import('../pages/test.vue' /* webpackChunkName: "pages/test" */))
+const _5225e62d = () => interopDefault(import('../pages/portfolio/communications-design/index.vue' /* webpackChunkName: "pages/portfolio/communications-design/index" */))
+const _c04cce6e = () => interopDefault(import('../pages/portfolio/design-research/index.vue' /* webpackChunkName: "pages/portfolio/design-research/index" */))
+const _5166fda6 = () => interopDefault(import('../pages/portfolio/ui-ux-design/index.vue' /* webpackChunkName: "pages/portfolio/ui-ux-design/index" */))
+const _3545f664 = () => interopDefault(import('../pages/portfolio/web-design/index.vue' /* webpackChunkName: "pages/portfolio/web-design/index" */))
+const _46713cd6 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+const _506f44e5 = () => interopDefault(import('../pages/portfolio/communications-design/_slug.vue' /* webpackChunkName: "pages/portfolio/communications-design/_slug" */))
+const _c3ba10fe = () => interopDefault(import('../pages/portfolio/design-research/_slug.vue' /* webpackChunkName: "pages/portfolio/design-research/_slug" */))
+const _4fb05c5e = () => interopDefault(import('../pages/portfolio/ui-ux-design/_slug.vue' /* webpackChunkName: "pages/portfolio/ui-ux-design/_slug" */))
+const _38b338f4 = () => interopDefault(import('../pages/portfolio/web-design/_slug.vue' /* webpackChunkName: "pages/portfolio/web-design/_slug" */))
+const _0e3e52f4 = () => interopDefault(import('../pages/portfolio/_slug.vue' /* webpackChunkName: "pages/portfolio/_slug" */))
 
 const emptyFn = () => {}
 
@@ -31,55 +31,55 @@ export const routerOptions = {
 
   routes: [{
     path: "/about",
-    component: _79d68257,
+    component: _85e0f260,
     name: "about"
   }, {
     path: "/portfolio",
-    component: _908a12d6,
+    component: _0ad11064,
     name: "portfolio"
   }, {
     path: "/test",
-    component: _ef6ff590,
+    component: _03dfe4c2,
     name: "test"
   }, {
     path: "/portfolio/communications-design",
-    component: _590505b4,
+    component: _5225e62d,
     name: "portfolio-communications-design"
   }, {
     path: "/portfolio/design-research",
-    component: _064ca990,
+    component: _c04cce6e,
     name: "portfolio-design-research"
   }, {
     path: "/portfolio/ui-ux-design",
-    component: _01c7fa3f,
+    component: _5166fda6,
     name: "portfolio-ui-ux-design"
   }, {
     path: "/portfolio/web-design",
-    component: _0ed653a7,
+    component: _3545f664,
     name: "portfolio-web-design"
   }, {
     path: "/",
-    component: _cce345c8,
+    component: _46713cd6,
     name: "index"
   }, {
     path: "/portfolio/communications-design/:slug?",
-    component: _574e646c,
+    component: _506f44e5,
     name: "portfolio-communications-design-slug"
   }, {
     path: "/portfolio/design-research/:slug?",
-    component: _04960848,
+    component: _c3ba10fe,
     name: "portfolio-design-research-slug"
   }, {
     path: "/portfolio/ui-ux-design/:slug?",
-    component: _001158f7,
+    component: _4fb05c5e,
     name: "portfolio-ui-ux-design-slug"
   }, {
     path: "/portfolio/web-design/:slug?",
-    component: _0d1fb25f,
+    component: _38b338f4,
     name: "portfolio-web-design-slug"
   }, {
     path: "/portfolio/:slug",
-    component: _93f75566,
+    component: _0e3e52f4,
     name: "portfolio-slug"
   }],
 
